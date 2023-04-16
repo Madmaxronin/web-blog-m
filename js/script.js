c23a713ec79dccb96d885367ce72d2cf77adc8ba
Mawell Ferrer <maxwellferrer1992@gmail.com>
2023-04-16 07:10:56 +0100

    navegacion al blog y buscador

--- js/script.js
+++ js/script.js
@@ -5,3 +5,48 @@ function mostrar_menu(){
     document.getElementById("move-content").classList.toggle('move-container-all');
     document.getElementById("show-menu").classList.toggle('show-menu-lateral');
 }
+
+
+
+
+
+
+
+
+
+
+                            //Buscador de contenido
+
+
+//Ejecutar funcion
+
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+
+//Declarando variables
+
+bars_search =          document.getElementById("ctn-bars-search");
+cover_ctn_search =     document.getElementById("cover-ctn-search");
+inputSearch =          document.getElementById("inputSearch");
+box_search =           document.getElementById("box-search");
+
+
+//Funcion para mostrar el buscador
+
+function mostrar_buscador(){
+
+    bars_search.style.top = "80px";
+    cover_ctn_search.style.display = "block";
+    inputSearch.focus();
+
+}
+
+
+//Funcion ocultar buscador
+
+function ocultar_buscador(){
+
+    bars_search.style.top = "-10px";
+    cover_ctn_search.style.display = "none";
+    inputSearch.value = "";
+}
